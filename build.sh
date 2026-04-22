0614fd3850ec17550ee9f0e555543d81d370b82f
lulboi30 <lulboi30@gmail.com>
2026-04-22 22:22:48 +0600

    forgot things start with 'ilb'

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ mkdir bin
 echo "Building Shared Object"
 
 gcc -c -std=c99 -fPIC climenu/climenu.c -o bin/climenu.o
-gcc -shared bin/climenu.o -o bin/climenu.so
+gcc -shared bin/climenu.o -o bin/libclimenu.so
 rm bin/climenu.o
 
 echo "Building Static Object"
 
 gcc -c -std=c99 climenu/climenu.c -o bin/climenu.o
-ar rcs bin/climenu.a bin/climenu.o
+ar rcs bin/climenu.a bin/libclimenu.o
 rm bin/climenu.o
 
 echo "Copy Header To 'bin' Folder? [y/n]"
